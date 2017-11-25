d74678b96898a38249d71a932980a3c782a35ff3
Andy Fleming <andy@andyfleming.com>
2017-11-24 21:46:32 -0800

    Simplifying arguments passed

--- src/index.js
+++ src/index.js
@@ -11,41 +11,42 @@ function interval(func, intervalLength, options = {}) {
     const settings = Object.assign(defaults, options)
 
     return new Promise((rootPromiseResolve, rootPromiseReject) => {
-        callFunction(func, intervalLength, settings, 1, rootPromiseResolve, rootPromiseReject)        
-    })
-}
 
-function callFunction(func, intervalLength, settings, currentIteration, rootPromiseResolve, rootPromiseReject) {
-
-    let stopRequested = false
-    const stop = () => {
-        stopRequested = true
-    }
-
-    const callNext = () => {
-        // If we've hit the desired number of iterations, or stop was called, resolve the root promise and return
-        if (currentIteration === settings.iterations || stopRequested) {
-            rootPromiseResolve()
-            return
+        const callFunction = currentIteration => {
+            
+            let stopRequested = false
+            const stop = () => {
+                stopRequested = true
+            }
+        
+            const callNext = () => {
+                // If we've hit the desired number of iterations, or stop was called, resolve the root promise and return
+                if (currentIteration === settings.iterations || stopRequested) {
+                    rootPromiseResolve()
+                    return
+                }
+        
+                // Otherwise, call the next iteration
+                callFunction(currentIteration + 1)
+            }
+        
+            const calculatedIntervalLength = (typeof intervalLength === 'function') ? intervalLength(currentIteration + 1) : intervalLength
+        
+            // Call the user function after the desired interval length. After, call the next iteration (and/or handle error)
+            setTimeout(() => {
+                func(currentIteration, stop).then(callNext).catch(err => {
+                    if (!settings.stopOnError) {
+                        callNext()
+                        return
+                    }
+        
+                    rootPromiseReject(err)
+                })
+            }, calculatedIntervalLength)
         }
 
-        // Otherwise, call the next iteration
-        callFunction(func, intervalLength, settings, currentIteration + 1, rootPromiseResolve, rootPromiseReject)
-    }
-
-    const calculatedIntervalLength = (typeof intervalLength === 'function') ? intervalLength(currentIteration + 1) : intervalLength
-
-    // Call the user function after the desired interval length. After, call the next iteration (and/or handle error)
-    setTimeout(() => {
-        func(currentIteration, stop).then(callNext).catch(err => {
-            if (!settings.stopOnError) {
-                callNext()
-                return
-            }
-
-            rootPromiseReject(err)
-        })
-    }, calculatedIntervalLength)
+        callFunction(1)        
+    })
 }
 
 module.exports = interval
