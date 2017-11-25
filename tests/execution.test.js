4a3661b7bb85fc73fa784b0e9e30f5aa1c24c8d5
Andy Fleming <andy@andyfleming.com>
2017-11-24 20:17:17 -0800

    A couple of basic tests

---
+++ tests/execution.test.js
@@ -0,0 +1,35 @@
+const test = require('ava')
+const interval = require('../src/index')
+
+test.cb('User function sholud be called', t => {
+    t.plan(1)
+    
+    const userFunc = () => {
+        t.pass()
+        t.end()
+        return Promise.resolve()
+    }
+
+    interval(userFunc, 10)
+})
+
+test.cb('User function sholud be called exactly the desired number of iterations', t => {
+    t.plan(3)
+    
+    const userFunc = i => {
+        
+        // If the function was called for a 4th time, throw an error
+        if (i > 3) {
+            throw new Error('User func was called more than 3 times')
+        }
+
+        // Otherwise mark the pass
+        t.pass()
+        
+        return Promise.resolve()
+    }
+
+    interval(userFunc, 10, {iterations: 3}).then(() => {
+        t.end()
+    })
+})
