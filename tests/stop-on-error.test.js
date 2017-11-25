38e5d99df557deb935c281d78c180875f7bef276
Andy Fleming <andy@andyfleming.com>
2017-11-24 23:52:36 -0800

    Adding tests for option stopOnError

---
+++ tests/stop-on-error.test.js
@@ -0,0 +1,35 @@
+const test = require('ava')
+const interval = require('../src/index')
+
+test.cb('if true, should prevent further execution', t => {
+    t.plan(1)
+    
+    const userFunc = i => {
+        if (i > 1) {
+            t.fail('Execution should have stopped')
+        }
+        
+        return Promise.reject()
+    }
+
+    interval(userFunc, 10, {iterations: 3}).then(() => {
+        t.fail()
+    }).catch((err) => {
+        t.pass()
+        t.end()
+    })
+})
+
+test.cb('if false, option should prevent interval from stopping', t => {
+    t.plan(3)
+    
+    const userFunc = i => {
+        t.pass()
+        
+        return Promise.reject()
+    }
+
+    interval(userFunc, 10, {iterations: 3, stopOnError: false}).then(() => {
+        t.end()
+    })
+})
